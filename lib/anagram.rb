4ec314b14db77490e378c41d762ed24013a9d45e
alexander gubernikoff <agubernikoff@gmail.com>
2022-03-15 12:24:01 -0400

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match (array)
+        chars=@word.chars
+        matches =array.filter{|a| a.chars.sort == chars.sort}
+        return matches
+    end
+end
